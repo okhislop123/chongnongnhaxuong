99dd6775706977732703f5633bca5cd2e91da284
okhislop123 <huyh4496@gmail.com>
2022-08-28 18:45:49 +0700

    update

--- sources/tin-tuc.php
+++ sources/tin-tuc.php
@@ -140,7 +140,65 @@ $bg = $d->getTemplates(60);
                             <?php echo @$phantrang['paging'] ?>
                         </div>
                     <?php  } else { ?>
-                        test
+                        <div class="container__item">
+                            <div class="title_menu_priod row">
+                                <div class="tilo">
+                                    <?php foreach ($loai2 as $key => $item) { ?>
+                                        <div onclick="changeMenu(this)" data-id="<?= $item['id'] ?>" class="item-menu-prm2 <?= !$key ? "active" : "" ?>"><?= $item['ten_' . $lang] ?></div>
+                                    <?php } ?>
+                                </div>
+
+
+                                <?php foreach ($loai2 as $k => $value) {
+                                    $tintuc2 = $d->o_fet("select * from #_tintuc where hien_thi = 1  and id_loai = " . $value["id"] . " order by so_thu_tu asc, id desc");
+                                ?>
+                                    <div class="lo test <?= !$k ? "active" : "" ?>" data-id="<?= $value['id'] ?>">
+                                        <div class="item__cs__ba">
+                                            <div class="container__item__4">
+                                                <?php foreach ($tintuc2  as $i => $item) {
+                                                ?>
+                                                    <div class="col-item_0">
+                                                        <div class="content2l2l">
+                                                            <div class=" itemdetailnew5">
+                                                                <div class="col-12">
+                                                                    <div class="img-tintuc_2">
+                                                                        <a href="<?= URLPATH . $item['alias_' . $lang] ?>.html?lan=<?= $lang ?>" title="<?= $item['ten_' . $lang] ?>">
+                                                                            <img src="<?= URLPATH ?>thumb.php?src=<?= URLPATH ?>img_data/images/<?= $item['hinh_anh'] ?>&w=730&h=400" alt="<?= $item['ten_' . $lang] ?>" onerror="this.src='<?= URLPATH ?>templates/error/error.jpg';">
+                                                                        </a>
+                                                                        <div class="mota__ctv3">
+                                                                            <div class="groupvxa">
+                                                                                <h3 style="margin: 0;color:orange"><a style="color: orange;" href="<?= URLPATH . $item['alias_' . $lang] ?>.html?lan=<?= $lang ?>" title="$item['ten_'.$lang] ?>"><?= $item['ten_' . $lang] ?></a></h3>
+
+                                                                                <img src="<?= URLPATH . 'templates/images/proj-line.png' ?>" alt="line">
+
+
+                                                                            </div>
+
+                                                                        </div>
+
+                                                                    </div>
+                                                                </div>
+
+                                                            </div>
+
+                                                        </div>
+
+                                                    </div>
+
+                                                <?php } ?>
+                                            </div>
+                                        </div>
+
+                                        <div class="pagination-page">
+                                            <?php echo @$phantrang['paging'] ?>
+                                        </div>
+                                    </div>
+                                <?php } ?>
+
+                            </div>
+                        </div>
+                        <br><br>
+
                     <?php } ?>
                 <?php } ?>
             <?php } ?>
